56436c593ffdfefafd9d40ab12cf6e3ff3de0afa
queuing4oranges <katjazenker@gmail.com>
2025-04-19 22:06:19 +0200

    Request for adding transactions

--- src/PaymentModal.jsx
+++ src/PaymentModal.jsx
@@ -1,27 +1,69 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import {
 	Modal,
 	ModalHeader,
 	ModalBody,
 	ModalFooter,
 	Button,
-	Input
+	Form, FormGroup, Input, Label
 } from 'reactstrap';
+import { apiClient } from '../api';
 
 export default function PaymentModal({ setPaymentModalShow }) {
-return (
-	<Modal isOpen={true} toggle={() => {}}>
-		<ModalHeader>Payment</ModalHeader>
-		<ModalBody>
-			<Input
-				type='number'
-				placeholder='Amount'
-			/>
-		</ModalBody>
-		<ModalFooter>
-			<Button color='primary'>Submit</Button>
-			<Button onClick={() => setPaymentModalShow(false)} color='secondary'>Cancel</Button>
-		</ModalFooter>
-	</Modal>
-)
+	const { register, handleSubmit } = useForm();
+
+	const amountField = register('amount');
+	const noteField = register('note');
+
+	const addTransaction = async (values) => {
+		console.log(values, 'values')
+		try {
+			const response = await apiClient.post('/transactions/create.php', {
+				amount: values.amount,
+				note: values.note
+			});
+
+			if(response.status !== 200) {
+				throw new Error(`Something went wrong. Response status: ${response.status}`);
+			}
+
+			setPaymentModalShow(false);
+		} catch {
+			console.error('Could not add transaction');
+		}
+	}
+
+	return (
+		<Modal isOpen={true} toggle={() => {}}>
+			<ModalHeader>Neue Transaktion</ModalHeader>
+			<ModalBody>
+				<Form onSubmit={handleSubmit(addTransaction)}>
+					<FormGroup>
+						<Label>Betrag eingeben</Label>
+						<input
+							className='form-control'
+							type='number'
+							placeholder='Betrag'
+							id='amount'
+							name='amount'
+							{...amountField}
+						/>
+					</FormGroup>
+					<FormGroup>
+						<Label>Notiz hinzufügen</Label>
+						<input
+							className='form-control'
+							id='note'
+							name='note'
+							{...noteField}
+							type='text'
+						/>
+					</FormGroup>
+					<Button type='submit' color='primary'>Bestätigen</Button>
+					<Button onClick={() => setPaymentModalShow(false)} color='secondary'>Cancel</Button>
+				</Form>
+			</ModalBody>
+		</Modal>
+	)
 }
