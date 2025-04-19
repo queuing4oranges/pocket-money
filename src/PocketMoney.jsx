c505a631577d3a0e684b16ce7e19199dd5af1191
queuing4oranges <katjazenker@gmail.com>
2025-04-19 18:23:52 +0200

    Add background, initial styling

--- src/PocketMoney.jsx
+++ src/PocketMoney.jsx
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Row, Col, Button, Form, Input, Label } from 'reactstrap';
 
+import './pocketmoney.scss';
+
 export default function PocketMoney() {
-return (
-	<Container className='pocket-money-container'>
-	<Row>
-		<Col>
-		<h1>Pocket Money</h1>
-		<p>Manage your pocket money with ease!</p>
-		</Col>
-	</Row>
-	<Row>
-		<Col>
-		<Form>
-			<Label for="amount">Amount</Label>
-			<Input type="number" id="amount" placeholder="Enter amount" />
-			<Button color="primary">Add</Button>
-		</Form>
-		</Col>
-	</Row>
-	</Container>
-)
+	return (
+		<Container fluid className='h-100 w-100 pocket-money-container'>
+		<Row>
+			<Col>
+			<h1>Pocket Money</h1>
+			<p>Manage your pocket money with ease!</p>
+			</Col>
+		</Row>
+		<Row>
+			<Col>
+			<Form>
+				<Label for="amount">Amount</Label>
+				<Input type="number" id="amount" placeholder="Enter amount" />
+				<Button color="primary">Add</Button>
+			</Form>
+			</Col>
+		</Row>
+		</Container>
+	)
 }
